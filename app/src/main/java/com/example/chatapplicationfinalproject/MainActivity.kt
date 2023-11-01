0ac5f32d391bfa92ce1f92e37862efa417e5850d
Irfan hossain <irfanhpssain@gmail.com>
2023-11-01 18:30:40 +0600

    RecycalerView Add and Working

--- app/src/main/java/com/example/chatapplicationfinalproject/MainActivity.kt
+++ app/src/main/java/com/example/chatapplicationfinalproject/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.chatapplicationfinalproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.chatapplicationfinalproject.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var userList :ArrayList<User>
+    private lateinit var adapter: UserAdapter
+    private lateinit var mAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        mAuth = FirebaseAuth.getInstance()
+
+        userList = ArrayList()
+        adapter = UserAdapter(this, userList)
+
+
     }
 }
--- app/src/main/java/com/example/chatapplicationfinalproject/SignUp.kt
+++ app/src/main/java/com/example/chatapplicationfinalproject/SignUp.kt
@@ -8,10 +8,13 @@ import android.util.Log
 import android.widget.Toast
 import com.example.chatapplicationfinalproject.databinding.ActivitySignUpBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class SignUp : AppCompatActivity() {
     private lateinit var binding: ActivitySignUpBinding
     private lateinit var mAuth: FirebaseAuth
+    private lateinit var mDbRef: DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivitySignUpBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
@@ -30,29 +33,35 @@ class SignUp : AppCompatActivity() {
         }
     //...SignUp button in SignUp page....
         binding.signUpBtn.setOnClickListener {
-            val username = binding.singupUsernameEtxt.text.toString()
+            val name = binding.singupUsernameEtxt.text.toString()
             val email = binding.singupEmailEtxt.text.toString()
             val password = binding.singupPasswordEtxt.text.toString()
             
             //Create a Methord for SignUp.....
-            signup(username,email,password)
+            signup(name,email,password)
         }
     //....................................    
     }
 
-    private fun signup(username: String, email: String, password: String) {
+    private fun signup(name: String, email: String, password: String) {
         //Logic for Creating user
 
         mAuth.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
                     //code for juming to home
-
+                    addUserToDatabase(name,email,mAuth.currentUser?.uid!!)
                     val intent = Intent(this@SignUp,MainActivity::class.java)
                     startActivity(intent)
                 } else {
                     Toast.makeText(this@SignUp,"SOme error occurred",Toast.LENGTH_SHORT).show()
                 }
             }
+    }
+
+    private fun addUserToDatabase(name: String, email: String, uid: String) {
+        //intilize realtime database
+        mDbRef = FirebaseDatabase.getInstance().getReference()
+        mDbRef.child("user").child(uid).setValue(User(name,email,uid))
     }
 }
